773fcb9c09a6b64432573ca81dbbb7ebc18fc4be
noakesbeer <132654535+noakesbeer@users.noreply.github.com>
2023-05-05 17:04:06 +0100

    2 files added

---
+++ environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCJ3JTM9qz1yRefAKTV8CprmOLgv_l4FOU",
+    authDomain: "noakesbeer.firebaseapp.com",
+    projectId: "noakesbeer",
+    storageBucket: "noakesbeer.appspot.com",
+    messagingSenderId: "861468273473",
+    appId: "1:861468273473:web:f7392dc2736e1a8afecfc0",
+    measurementId: "G-86B4EQRK97"
+  }
+};
+
+
